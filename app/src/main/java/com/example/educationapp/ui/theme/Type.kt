51c0275f84663eebd2a75572fca5020b80954d85
Paulo Ricardo <pauloricardo.jesus2011@gmail.com>
2024-06-17 10:33:01 -0300

    Adding profile photo to the drawable folder and adding inter font to the font folder

--- app/src/main/java/com/example/educationapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/educationapp/ui/theme/Type.kt
@@ -2,9 +2,17 @@ package com.example.educationapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.educationapp.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
