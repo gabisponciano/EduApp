88c9c58400b80fe939c2a09914c4500a6b663985
Paulo Ricardo <pauloricardo.jesus2011@gmail.com>
2024-06-17 09:46:43 -0300

    Colors Added

--- app/src/main/java/com/example/educationapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/educationapp/ui/theme/Color.kt
@@ -2,18 +2,14 @@ package com.example.educationapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Primary_Green = Color(0xFF5DB075)
+val TextFieldBackground = Color(0xFFE8E8E8)
+val TextFieldText = Color(0xFFBDBDBD)
 
-
-
-
-
-
-
+// Android Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
